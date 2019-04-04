61516f6ab69ab5d7d1fed3d43ce2785d32e4c772
Mohamed Sididque <mmohamedsiddique@hawk.iit.edu>
2019-04-03 19:40:25 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,5 +3,17 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
-function
+function getAge(bday) {
+    var today = new Date();
+    var birthDate = new Date(bday);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 
+if(getAge("27/06/1989") >= 18) {
+    alert("Sorry you cannot be under 18 to enter this promotion");
+} 
