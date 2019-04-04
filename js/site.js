fed5daecb65dadbc5cdd35f10dec85c1a8d5d0b0
Mohamed Sididque <mmohamedsiddique@hawk.iit.edu>
2019-04-03 21:00:38 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -7,10 +7,10 @@ function confirm() {
 var button1 = document.getElementById("radio1");
 var button2 = document.getElementById("radio2");
 
-if (button1.checked){
+if (radio1.checked){
    alert("Thank you for signing up for our newsletter!\n" +
     "We will update you as soon as new sales season starts!");
-}else if (button2.checked) {
+}else if (radio2.checked) {
     alert("Sorry, to hear you are leaving Android Phone\n" +
     "Thank you for signing up for our newsletter!\n" +
     "We will update you as soon as new sales season starts!");
