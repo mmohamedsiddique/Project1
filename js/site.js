8ae11e4e14dbfc8f596b64b3fa05db075869082d
Mohamed Sididque <mmohamedsiddique@hawk.iit.edu>
2019-04-03 20:55:26 -0500

    Changes

--- js/site.js
+++ js/site.js
@@ -4,14 +4,16 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 function confirm() {
-var x = document.getElementById("bday").value;
-if (x < 18, "year") {
-alert("Your format is incorrect, please enter is at mm/dd/yyyy or choose from the calendar");
-return false;
-}
-else
-{
-  alert("Thank you for signing up for our newsletter!\n" +
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+
+if (button1.checked){
+   alert("Thank you for signing up for our newsletter!\n" +
+    "We will update you as soon as new sales season starts!");
+}else if (button2.checked) {
+    alert("Sorry, to hear you are leaving Android Phone\n" +
+    "Thank you for signing up for our newsletter!\n" +
     "We will update you as soon as new sales season starts!");
 }
+  
 }
